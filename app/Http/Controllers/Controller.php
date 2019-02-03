8da0620894f5a6957f4b8600af3970b426f23f4a
Patrick Stearns <pdeans1986@gmail.com>
2019-02-03 09:32:54 -0800

    Added support for search filter

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use Exception;
 use Illuminate\Http\Request;
 use Laravel\Lumen\Routing\Controller as BaseController;
 use pdeans\Miva\Api\Manager as Api;
@@ -40,6 +41,32 @@ class Controller extends BaseController
             $api->show($request->input('show'));
         }
 
+        if ($request->has('search')) {
+            $searches = $request->input('search');
+
+            if (is_array($searches)) {
+                foreach ($searches as $search) {
+                    if (empty($search['values']) || !is_array($search['values'])) {
+                        throw new Exception('Invalid search criteria provided.');
+                    }
+
+                    if (count($search['values']) === 1) {
+                        if (!$this->validateSearchFields($search['values'][0])) {
+                            throw new Exception('Invalid search criteria provided.');
+                        }
+
+                        $api->search(
+                            $search['values'][0]['field'],
+                            $search['values'][0]['operator'],
+                            ($search['values'][0]['value'] ?? null)
+                        );
+                    } else {
+                        $api->search($this->getSearchFieldValues($search['values']));
+                    }
+                }
+            }
+        }
+
         return $api;
     }
 
@@ -50,5 +77,83 @@ class Controller extends BaseController
         }
 
         return $api;
+    }
+
+    protected function getSearchFieldValues(array $search_fields)
+    {
+        $search_field_vals = [];
+
+        foreach ($search_fields as $field => $value) {
+            if (is_int($field)) {
+                if (!$this->validateSearchFields($value)) {
+                    throw new Exception('Invalid search criteria provided.');
+                }
+
+                $field_vals = [
+                    'field'    => $value['field'],
+                    'operator' => strtoupper($value['operator']),
+                ];
+
+                if ($indexed_vals = array_filter(array_keys($value), 'is_int')) {
+                    foreach (array_values($indexed_vals) as $index_val) {
+                        $field_vals['value'][] = $this->getSearchFieldValues($value[$index_val]);
+                    }
+                } elseif (isset($value['value'])) {
+                    $field_vals['value'] = $value['value'];
+                }
+
+                if (!isset($search_field_vals['field']) && !isset($search_field_vals['operator'])) {
+                    $search_field_vals[] = $field_vals;
+                }
+            } else {
+                if (!$this->validateSearchField($field)) {
+                    throw new Exception('Invalid search criteria provided.');
+                }
+
+                if ($field === 'value' && $indexed_vals = array_filter(array_keys($search_fields), 'is_int')) {
+                    $search_field_vals['value'] = [];
+
+                    foreach (array_values($indexed_vals) as $index_val) {
+                        $search_field_vals['value'][] = $this->getSearchFieldValues($search_fields[$index_val], true);
+                    }
+                } elseif (!is_int($field)) {
+                    $search_field_vals[$field] = ($field === 'operator' ? strtoupper($value) : $value);
+                }
+            }
+        }
+
+        return $search_field_vals;
+    }
+
+    protected function validateSearchFields(array $search_fields)
+    {
+        if (!isset($search_fields['field']) || !isset($search_fields['operator'])) {
+            return false;
+        }
+
+        if (
+            !isset($search_fields['value']) &&
+            !in_array(strtoupper($search_fields['operator']), $this->getSearchNullOperators())
+
+        ) {
+            return false;
+        }
+
+        return true;
+    }
+
+    protected function validateSearchField(string $field)
+    {
+        return in_array($field, $this->getSearchFieldKeys());
+    }
+
+    protected function getSearchFieldKeys()
+    {
+        return ['field', 'operator', 'value'];
+    }
+
+    protected function getSearchNullOperators()
+    {
+        return ['TRUE', 'FALSE', 'NULL'];
     }
 }
